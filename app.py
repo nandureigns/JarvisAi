8b8ce7b130b97d37b9b6984880dc6ffc8b35634e
nandureigns <139434750+nandureigns@users.noreply.github.com>
2025-06-05 10:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from fastapi import FastAPI, Request
 import os
 import pytz
 
-TOKEN = os.getenv('7358758559:AAEWhzrwLx5PyBEI5xv1W3P67yWeNdIFga4')
+TOKEN = os.getenv('')
 app = FastAPI()
 chat_memory = {}
 
